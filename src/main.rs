ae91a0ad9e2c1c5026e616ea56144aaf885eb08a
Joseph Hejderup <jhejderup@users.noreply.github.com>
2017-12-12 14:05:50 +0100

    no need on semver anymore, we like hello world

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
-extern crate semver;
-
-
-use semver::Version;
-use semver::VersionReq;
-
-
 fn main() {
-    let r = VersionReq::parse(">= 2.0.0").unwrap();
-    let v = Version::parse("1.0.0").unwrap();
-
-    let r1 = VersionReq::parse(">= 1.0.0").unwrap();
-    let v1 = Version::parse("1.0.0").unwrap();
-    
-    
-    assert!(r.to_string() == ">= 2.0.0".to_string());
-    assert!(r.matches(&v));
-    assert!(r1.to_string() == ">= 1.0.0".to_string());
-    assert!(r1.matches(&v1));
+    println!("Hell0, world!");
     
 }
 
