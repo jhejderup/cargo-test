7d825a7c9ee946cb9f8662a133c1e05c87de6cf5
Joseph Hejderup <jhejderup@users.noreply.github.com>
2017-12-12 14:03:08 +0100

    Add previos case from before

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,14 @@ use semver::VersionReq;
 fn main() {
     let r = VersionReq::parse(">= 2.0.0").unwrap();
     let v = Version::parse("1.0.0").unwrap();
+
+    let r1 = VersionReq::parse(">= 1.0.0").unwrap();
+    let v1 = Version::parse("1.0.0").unwrap();
+    
     
     assert!(r.to_string() == ">= 2.0.0".to_string());
     assert!(r.matches(&v));
+    assert!(r1.matches(&v1));
 }
 
 
