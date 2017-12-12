ebb5ef8e7e9ab9d4a2714add06d4cd662dfd3be2
Joseph Hejderup <jhejderup@users.noreply.github.com>
2017-12-12 14:01:40 +0100

    Support semver parse

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+extern crate semver;
+
+
+use semver::Version;
+use semver::VersionReq;
+
+
+fn main() {
+    let r = VersionReq::parse(">= 1.0.0").unwrap();
+    let v = Version::parse("1.0.0").unwrap();
+    
+    assert!(r.to_string() == ">= 1.0.0".to_string());
+    assert!(r.matches(&v));
+    println!("Hello, world!");
+}
+
+
+
+
