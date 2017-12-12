cb0089edc6f38b2efaa80c9c6d678de79c5d20fa
Joseph Hejderup <jhejderup@users.noreply.github.com>
2017-12-12 14:02:24 +0100

    Check for a different version

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use semver::VersionReq;
 
 
 fn main() {
-    let r = VersionReq::parse(">= 1.0.0").unwrap();
+    let r = VersionReq::parse(">= 2.0.0").unwrap();
     let v = Version::parse("1.0.0").unwrap();
     
-    assert!(r.to_string() == ">= 1.0.0".to_string());
+    assert!(r.to_string() == ">= 2.0.0".to_string());
     assert!(r.matches(&v));
 }
 
