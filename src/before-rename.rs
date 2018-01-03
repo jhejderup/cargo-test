10229baf796b8c5537f41f0d414c5be8dc818de5
Joseph Hejderup <j.i.hejderup@tudelft.nl>
2018-01-03 01:22:03 +0100

    Create before-rename.rs

---
+++ src/before-rename.rs
@@ -0,0 +1,11 @@
+extern crate rand;
+
+
+fn tes() {
+    let tuple = rand::random::<(f64, char)>();
+    println!("{:?}", tuple);
+    let tuple2 = rand::random::<(f64, char)>();
+    println!("{:?}", tuple2);
+
+    
+}
